0180f81bcfe2d11791019e508516bd6e53b07c6c
Kriya Patel <kriya020805@gmail.com>
2026-05-18 14:53:07 +0530

    Assessment submission

---
+++ Assessment-2/Divide two numbers and print the quotient and remainder separately.py
@@ -0,0 +1,9 @@
+#Divide two numbers and print the quotient and remainder separately
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+quotient = a // b
+remainder = a % b
+
+print("the quotient is: ", quotient)
+print("the remainder is: ",remainder)
