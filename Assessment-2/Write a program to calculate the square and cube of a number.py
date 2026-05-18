6a26c4485d5f9598f98ba6ae802dba750b57311c
Kriya Patel <kriya020805@gmail.com>
2026-05-18 12:24:36 +0530

    Assessment submission

---
+++ Assessment-2/Write a program to calculate the square and cube of a number.py
@@ -0,0 +1,10 @@
+# Write a program to calculate the square and cube of a number.
+num = int(input("Enter a number: "))
+square = num ** 2
+cube = num **3
+print(f"the square of {num} is {square}")
+print(f"the cube of {num} is {cube}")
+
+
+
+
