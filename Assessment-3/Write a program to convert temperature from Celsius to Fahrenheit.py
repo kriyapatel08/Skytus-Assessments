5a1d204c9ae1adea5ff1bb810d7d4fcc93a09c9d
Kriya Patel <kriya020805@gmail.com>
2026-05-18 20:29:26 +0530

    Assessment submission

---
+++ Assessment-3/Write a program to convert temperature from Celsius to Fahrenheit.py
@@ -0,0 +1,11 @@
+#Write a program to convert temperature from Celsius to Fahrenheit.
+celsius = float(input("enter the temp in celsius: "))
+ 
+temerature = (celsius * 9/5) + 32
+
+print("temprature in Fahrenheit: ",temerature)
+ 
+ 
+
+
+
