47039ee03ce541edcef323cf2b423e946c6d9f70
Kriya Patel <kriya020805@gmail.com>
2026-05-18 12:28:49 +0530

    Assessment submission

---
+++ Assessment-2/Check if two entered numbers are equal.py
@@ -0,0 +1,8 @@
+# Check if two entered numbers are equal
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 == num2:
+    print("the number is equal")
+else:
+    print("the number is not equal")
