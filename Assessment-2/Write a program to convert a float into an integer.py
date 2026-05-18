a988ee2d3b2f77d0f406921b3de51da157a1ab44
Kriya Patel <kriya020805@gmail.com>
2026-05-18 15:05:48 +0530

    Assessment submission

--- Assessment-2/Write a program to convert a float into an integer.py
+++ Assessment-2/Write a program to convert a float into an integer.py
@@ -1,5 +1,6 @@
 # Write a program to convert a float into an integer.
 num = float(input("Enter a float number: "))
 integer_num = int(num)
-print(f"The integer value of {num} is {integer_num}")
+print("The integer value of", num, "is:", integer_num)
+
 
