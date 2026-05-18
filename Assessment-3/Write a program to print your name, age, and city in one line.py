2890afdb211c7139175084dc514fc199c3bad98c
Kriya Patel <kriya020805@gmail.com>
2026-05-18 15:20:17 +0530

    Assessment submission

--- Assessment-3/Write a program to print your name, age, and city in one line.py
+++ Assessment-3/Write a program to print your name, age, and city in one line.py
@@ -1,2 +1,8 @@
 #Write a program to print your name, age, and city in one .
-name = input("enter your name: ")
+name = str(input("enter your name: "))
+Age = int(input("enter your age: "))
+city = str(input("enter your city: "))
+
+print("my name is: ",name, "my age is: ",Age, "and I live in: ",city)
+
+
