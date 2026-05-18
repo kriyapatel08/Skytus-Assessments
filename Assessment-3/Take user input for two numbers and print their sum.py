495f73cfdae28f42cca2ceaed0d9bdad681461d2
Kriya Patel <kriya020805@gmail.com>
2026-05-18 15:56:01 +0530

    Assessment submission

---
+++ Assessment-3/Take user input for two numbers and print their sum.py
@@ -0,0 +1,7 @@
+#Take user input for two numbers and print their sum.
+n1 = int(input("enter a first number: "))
+n2 = int(input("enter a second number: "))
+
+num = n1 + n2
+ 
+print("the result is: ",num)
