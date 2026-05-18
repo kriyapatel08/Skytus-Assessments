309d2a95ee090bc77826a9088980ea01181963ea
Kriya Patel <kriya020805@gmail.com>
2026-05-18 14:42:22 +0530

    Assessment submission

---
+++ Assessment-2/Take a number as a string, convert it to an integer, and multiply it by 10.py
@@ -0,0 +1,6 @@
+#Take a number as a string, convert it to an integer, and multiply it by 10.
+num_str = input("Enter a number as a string: ")
+num_int = int(num_str)
+result = num_int * 10
+print("The result of multiplying by 10 is: ", result)
+
