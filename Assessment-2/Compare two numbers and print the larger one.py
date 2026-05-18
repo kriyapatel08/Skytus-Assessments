4cfea6303d5757cc5a2b5a054c46e4747c90e18b
Kriya Patel <kriya020805@gmail.com>
2026-05-18 12:09:20 +0530

    Assessment submission

---
+++ Assessment-2/Compare two numbers and print the larger one.py
@@ -0,0 +1,10 @@
+#Compare two numbers and print the larger one.
+#Get user input for the two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1>num2:
+    print("the larger number is: ", num1)
+else:
+    print("the larger number is: ", num2)
+    
