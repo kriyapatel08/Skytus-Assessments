da99fd489c866b4a63ee30e7142f24b23a6c9fb5
Kriya Patel <kriya020805@gmail.com>
2026-05-19 15:44:26 +0530

    Assessment submission

---
+++ Assessment-5/Add a new fruit to the set.py
@@ -0,0 +1,7 @@
+#Add a new fruit to the set.
+tuple = ("mango","chiku","apple","banana","graps")
+
+a = list(tuple)
+a.append("orange")
+
+print("a new tuple is: ",a)
---
+++ Assessment-5/Create a set of 5 fruits.py
@@ -0,0 +1,4 @@
+#Create a set of 5 fruits.
+tuple = ("mango","chiku","apple","banana","graps")
+
+print("the setb of 5 fruits is: ",tuple)
