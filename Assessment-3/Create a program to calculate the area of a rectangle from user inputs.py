db6621fadd552577f61538a1dcd587241d77afda
Kriya Patel <kriya020805@gmail.com>
2026-05-19 10:25:09 +0530

    Assessment submission

---
+++ Assessment-3/Create a program to calculate the area of a rectangle from user inputs.py
@@ -0,0 +1,7 @@
+#Create a program to calculate the area of a rectangle from user inputs.
+length = float(input("enter the length of the rectangle: "))
+width = float(input("enter the width of the rectangle: "))
+
+area = length * width
+print("the area of the rectangle is: ", area)
+
---
+++ Assessment-3/Write a program to swap the values of two variables.py
@@ -0,0 +1,11 @@
+#Write a program to swap the values of two variables.
+
+a = input("enter a value for a: ")
+b = input("enter a value for b: ")
+
+temp = a
+a = b
+b = temp
+print("the value of a is: ",a)
+print("the value of b is: ",b)
+    
