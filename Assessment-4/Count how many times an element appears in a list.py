8636fb9324801ebf6aa07445ec381c2b04fda0a8
Kriya Patel <kriya020805@gmail.com>
2026-05-19 15:06:46 +0530

    Assessment submission

---
+++ Assessment-4/Count how many times an element appears in a list.py
@@ -0,0 +1,5 @@
+#Count how many times an element appears in a list.
+num = [12,21,54,85,74,21,45,78,21]
+element = 21
+count = num.count(element)
+print("The element appears", count, "times in the list.")
