0ac424b0cda0654043d01529b57dca927d7037de
Kriya Patel <kriya020805@gmail.com>
2026-05-19 14:24:44 +0530

    Assessment submission

--- Assessment-4/Add a new movie to the list.py
+++ Assessment-4/Add a new movie to the list.py
@@ -1,8 +1,4 @@
 #Add a new movie to the list.
-favorite_movies = []
-for i in range(3):
-    movie = input("Enter the name of one of your favorite movies: ")
-    favorite_movies.append(movie)   
-new_movie = input("Enter the name of a new movie to add to the list: ")
-favorite_movies.append(new_movie)
-print("Updated list of your favorite movies:", favorite_movies)
+favorite_movies = ["KGF","RRR","Vikram","Pushpa","Dhurandhar"]
+favorite_movies.append("Sarkaru Vaari Paata")
+print("Updated list of your favorite movies: ", favorite_movies)
--- Assessment-4/Create a list of your 5 favorite movies.py
+++ Assessment-4/Create a list of your 5 favorite movies.py
@@ -1,7 +1,3 @@
 #Create a list of your 5 favorite movies.
-favorite_movies = []
-for i in range(5):
-    movie = input("Enter the name of one of your favorite movies: ")
-    favorite_movies.append(movie)
-
-print("Your favorite movies are:", favorite_movies)
+favorite_movies = ["KGF","RRR","Vikram","Pushpa","Dhurandhar"]
+print(" five favourite movies are:", favorite_movies)
