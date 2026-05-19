b117730c6b4d20475184a05fb55b14e89fd8c5a0
Kriya Patel <kriya020805@gmail.com>
2026-05-19 15:02:41 +0530

    Assessment submission

---
+++ Assessment-4/Create a nested list and access a specific inner element.py
@@ -0,0 +1,6 @@
+#Create a nested list and access a specific inner element.
+nested_list = [[1, 2, 3], ['a', 'b', 'c'], [4.5, 5.6, 6.7]]
+
+# Accessing a specific inner element
+element = nested_list[1][1]
+print("The specific inner element is: ", element)
