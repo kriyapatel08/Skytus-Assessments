400f82e2cbf198f52917751e7f88bcaf1d2138d8
Kriya Patel <kriya020805@gmail.com>
2026-05-19 10:20:43 +0530

    Assessment submission

---
+++ Assessment-3/Ask the user for their birth year and calculate their current age.py
@@ -0,0 +1,7 @@
+#Ask the user for their birth year and calculate their current age.
+
+birth_year = int(input("enter your birth year: "))
+current_year = 2026
+age = current_year - birth_year
+print("your current age is: ",age)
+
