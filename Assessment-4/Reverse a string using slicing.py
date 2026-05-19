f23781f8a7463de07bcf87cbcb15e47ae4ffb6de
Kriya Patel <kriya020805@gmail.com>
2026-05-19 15:14:59 +0530

    Assessment submission

--- Assessment-4/Reverse a string using slicing.py
+++ Assessment-4/Reverse a string using slicing.py
@@ -1,4 +1,5 @@
 #Reverse a string using slicing.
 string = input("enter a string: ")
+
 reversed_string = string[::-1]              
 print("Reversed string:", reversed_string)
---
+++ Assessment-5/Create a tuple with 5 numbers.py
@@ -0,0 +1,3 @@
+#Create a tuple with 5 numbers.
+numbers = (10, 20, 30, 40, 50)
+print("The tuple with 5 numbers is: ", numbers)
