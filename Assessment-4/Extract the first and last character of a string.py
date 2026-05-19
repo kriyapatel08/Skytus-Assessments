775c683c01b471c8be7ebe89cb06b10ade8da45f
Kriya Patel <kriya020805@gmail.com>
2026-05-19 11:53:24 +0530

    Removed nested folder

---
+++ Assessment-4/Extract the first and last character of a string.py
@@ -0,0 +1,6 @@
+#Extract the first and last character of a string.
+string = input("enter a string: ")
+first_char = string [0]
+last_char = string[-1]
+print("First character:", first_char)
+print("Last character:", last_char)
