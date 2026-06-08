5f4be5aae472c7074d96e623287a5524e859cfd3
Kriya Patel <kriya020805@gmail.com>
2026-06-08 10:24:12 +0530

    Assessment submission

---
+++ Assessment-10/Error Handling.py
@@ -0,0 +1,11 @@
+#Write a program to handle division by zero error.
+try:
+    num1 = int(input("Enter numerator: "))
+    num2 = int(input("Enter denominator: "))
+
+    result = num1 / num2
+    print("Result =", result)
+
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+    
